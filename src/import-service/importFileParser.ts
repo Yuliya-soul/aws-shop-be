5b63058bb50c9e2cf4b37ccaf1b0d0bb814c7348
Your Name <darki@tut.by>
2021-08-22 13:14:05 +0300

    feat:Update the importFileParser lambda function (TASK 5) to send each CSV record into SQS

--- src/import-service/importFileParser.ts
+++ src/import-service/importFileParser.ts
@@ -4,38 +4,50 @@ import AWS from "aws-sdk";
 export const importFileParser = async (event) => {
   const BUCKET = "import-service-5";
   const s3 = new AWS.S3({ region: "us-east-1" });
+  var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 
   try {
     for (const record of event.Records) {
+      const keyRecord = decodeURI(record.s3.object.key);
       await new Promise<void>((resolve, reject) => {
         s3.getObject({
           Bucket: BUCKET,
-          Key: decodeURI(record.s3.object.key),
+          Key: keyRecord,
         })
           .createReadStream()
           .pipe(csv())
-          .on("data", (data) => console.log("Parsed new data", data))
+          .on("data", (data) =>
+            sqs.sendMessage(
+              {
+                QueueUrl: process.env.THE_QUEUE_URL,
+                MessageBody: JSON.stringify(data),
+              },
+              (err, data) => {
+                if (err) console.log(err, err.stack);
+                else console.log(data);
+              }
+            )
+          )
           .on("end", () => resolve())
           .on("error", (error) => reject(error));
       });
+
       await s3
         .copyObject({
           Bucket: BUCKET,
-          CopySource: `${BUCKET}/${record.s3.object.key}`,
-          Key: decodeURI(record.s3.object.key).replace("uploaded", "parsed"),
+          CopySource: `${BUCKET}/${keyRecord}`,
+          Key: keyRecord.replace("uploaded", "parsed"),
         })
         .promise();
       await s3
         .deleteObject({
           Bucket: BUCKET,
-          Key: decodeURI(record.s3.object.key),
+          Key: keyRecord,
         })
         .promise();
 
       console.log(
-        `${decodeURI(
-          record.s3.object.key
-        )} has been moved to the /parsed folder`
+        `${decodeURI(keyRecord)} has been moved to the /parsed folder`
       );
     }
   } catch (error) {
--- src/import-service/importProductsFile.ts
+++ src/import-service/importProductsFile.ts
@@ -1,14 +1,15 @@
 import { errorResponse, successResponse } from "./src/utils/apiResponseBuilder";
-import AWS from "aws-sdk";
+import * as AWS from "aws-sdk";
 
 export const importProductsFile = async (event) => {
   try {
+    const BUCKET = 'import-service-5';
     const s3 = new AWS.S3({ region: "us-east-1", signatureVersion: "v4" });
     const catalogName = event.queryStringParameters.name;
     const catalogPath = `uploaded/${catalogName}`;
 
     const params = {
-      Bucket: "import-service-5",
+      Bucket: BUCKET,
       Key: catalogPath,
     };
 
