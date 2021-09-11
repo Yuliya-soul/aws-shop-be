a3f6db41de0ca7adbbfc1d6b3e6899c6a63a88cc
Your Name <darki@tut.by>
2021-09-11 15:12:53 +0300

    fix:code

--- src/authorization-service/src/handlers/tokenAuthorizer.ts
+++ src/authorization-service/src/handlers/tokenAuthorizer.ts
@@ -6,8 +6,8 @@ exports.handler = function (event, context, callback) {
   }
   try {
     const token = event.authorizationToken;
-    const encodedCreds = token.split(" ")[1];
-    const buff = Buffer.from(encodedCreds, "base64");
+    const encodedCredentials = token.split(" ")[1];
+    const buff = Buffer.from(encodedCredentials, "base64");
     const plainCredits = buff.toString("utf-8").split(":");
     const userName = plainCredits[0];
     const password = plainCredits[1];
@@ -19,14 +19,14 @@ exports.handler = function (event, context, callback) {
     const effect =
       !storedUserPassword || storedUserPassword !== password ? "Deny" : "Allow";
 
-    const policy = generatePolicy(encodedCreds, effect, event.methodArn);
+    const policy = generatePolicy(encodedCredentials, effect, event.methodArn);
     callback(null, policy);
   } catch (e) {
     callback(`Unauthorized: ${e}`);
   }
 };
 
-const generatePolicy = (principalId, effect = "Allow", resource) => {
+const generatePolicy = (principalId, effect, resource) => {
   return {
     principalId: principalId,
     policyDocument: {
